d469fd3e80033846f98cc55d25a89bd2e9369aff
Rudimentary_Chef <akrish6@gatech.edu>
2024-02-23 17:04:56 -0500

    adds more stuff

--- code/FRED_Runner.py
+++ code/FRED_Runner.py
@@ -10,7 +10,7 @@ class Fred_Runner:
     def __init__(self,model_path):
         self.tokenizer, self.loaded_model = self.load_model_and_tokenizer(4, model_path)
 
-    def load_model_and_tokenizer(labels=4, model_path='/Users/adi/Desktop/RandomCode/Fred/bert_model.pth'):
+    def load_model_and_tokenizer(self, labels=4, model_path='/Users/adi/Desktop/RandomCode/Fred/bert_model.pth'):
         # Gets the tokenizer
         tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
@@ -52,7 +52,7 @@ class Fred_Runner:
         return df
     def predict_returnExcel(self,location_path,text_column, edit_orignal = True):
         df = pd.read_excel(location_path)
-        df = self.predict_column(df,text_column,location_path)
+        df = self.predict_column(df,text_column)
         if(edit_orignal == True):
             df.to_excel(location_path)
         else:
@@ -60,46 +60,53 @@ class Fred_Runner:
             df.to_excel(location_path)
 
     def predict_returnCSV(self,location_path,text_column, edit_orignal = True):
+        print("hello")
         df = pd.read_excel(location_path)
-        df = self.predict_column(df,text_column,location_path)
+        df = self.predict_column(df,text_column)
         if(edit_orignal == True):
             df.to_csv(location_path)
         else:
             location_path = location_path + "withPredictions"
             df.to_csv(location_path)
-    if __name__ == "__main__":
-        while True:
-            location_path = input("Please enter location of the xlsx file: ")
-            if location_path.endswith(".xlsx"):
-                break
-            else:
-                print("Invalid input. Please enter a valid path to an Excel file.")
-
-        while True:
-            text_column = input("Please enter the column name exactly that you want to predict the class of: ")
-            # Add any additional validation if needed
-            if text_column:  # Check if the input is not an empty string
-                break
-            else:
-                print("Invalid input. Please enter a non-empty column name.")
-
-        while True:
-            edit_orignal = input(
-                "Enter 'yes' if you want to edit the original, 'no' if you want to predict in a copy of the file: ")
-            edit_orignal_boolean = {"True": True, "False": False, "Yes": True, "yes": True, "No": False,
-                                    "no": False}.get(edit_orignal.lower())
-            if edit_orignal_boolean is not None:
-                break
-            else:
-                print("Invalid input. Please enter 'yes' or 'no'.")
-
-        while True:
-            ExcelOrCsv = input("Type 'excel' if you want it in an Excel file, 'csv' if you want it in a CSV file: ")
-            if ExcelOrCsv.lower() in ['excel', 'csv']:
-                break
-            else:
-                print("Invalid input. Please enter 'excel' or 'csv'.")
-        if(ExcelOrCsv == "excel"):
-            predict_returnExcel(location_path, text_column, edit_orignal_boolean)
+if __name__ == "__main__":
+    while True:
+        location_path = input("Please enter location of the xlsx file: ")
+        if location_path.endswith(".xlsx"):
+            break
         else:
-            predict_returnCSV(location_path, text_column, edit_orignal_boolean)
+            print("Invalid input. Please enter a valid path to an Excel file.")
+
+    while True:
+        text_column = input("Please enter the column name exactly that you want to predict the class of: ")
+        # Add any additional validation if needed
+        if text_column:  # Check if the input is not an empty string
+            break
+        else:
+            print("Invalid input. Please enter a non-empty column name.")
+
+    while True:
+        edit_orignal = input(
+            "Enter 'yes' if you want to edit the original, 'no' if you want to predict in a copy of the file: ")
+        edit_orignal_boolean = {"True": True, "False": False, "Yes": True, "yes": True, "No": False,
+                                "no": False}.get(edit_orignal.lower())
+        if edit_orignal_boolean is not None:
+            break
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
+    while True:
+        ExcelOrCsv = input("Type 'excel' if you want it in an Excel file, 'csv' if you want it in a CSV file: ")
+        if ExcelOrCsv.lower() in ['excel', 'csv']:
+            break
+        else:
+            print("Invalid input. Please enter 'excel' or 'csv'.")
+    model_path = input("where is the bert model located: ")
+    fr = Fred_Runner(model_path)
+    if(ExcelOrCsv == "excel"):
+        print("location path is ", location_path)
+        print("column", text_column)
+        fr.predict_returnExcel(location_path, text_column, edit_orignal_boolean)
+    elif (ExcelOrCsv == "csv"):
+        print("location path is ", location_path)
+        print("column", text_column)
+        fr.predict_returnCSV(location_path, text_column, edit_orignal_boolean)
